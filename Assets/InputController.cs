76c7f6e1017406f967e17bfbce6e34bbb4230dee
Viet Dao <viet.dao@jls.ch>
2018-11-16 17:37:15 +0100

    Basic terrain and player
    Preparation for actual content

---
+++ Assets/InputController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+    public Rigidbody2D player;
+
+    private void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetKey(KeyCode.A)) {
+            this.player.AddForce(Vector2.left * 10);
+        } else if (Input.GetKey(KeyCode.D)) {
+            this.player.AddForce(Vector2.right * 10);
+        }
+    }
+}
