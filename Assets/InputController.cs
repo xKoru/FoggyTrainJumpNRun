da7a4cb22d27ecd1c5b463e1ff40d98880a101b8
denniswalter82 <denniswalter@runbox.com>
2018-11-19 22:38:19 +0100

    Test eines Scripts

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -6,15 +6,17 @@ public class InputController : MonoBehaviour {
     public Rigidbody2D player;
 
     private void Start() {
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate () {
         if (Input.GetKey(KeyCode.A)) {
-            this.player.AddForce(Vector2.left * 10);
+            this.player.AddForce(Vector2.left * 25);
         } else if (Input.GetKey(KeyCode.D)) {
-            this.player.AddForce(Vector2.right * 10);
+            this.player.AddForce(Vector2.right * 25);
+        } else if (Input.GetKey(KeyCode.W)) {
+            this.player.AddForce(Vector2.up * 25);
         }
     }
 }
